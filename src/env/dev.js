a4302c51c935c1a3b1b5c9f683c5822c78d00a4c
Aditya Jain <adityajain@Adityas-MacBook-Pro.local>
2023-09-04 09:26:07 +0530

    a

--- src/env/dev.js
+++ src/env/dev.js
@@ -2,6 +2,6 @@ const devObj = {
     redirectUri: "http://localhost:3000/oauth2/redirect",
     apiBaseUrl: "http://ec2-52-9-207-36.us-west-1.compute.amazonaws.com:8080",
     // pythonapiBaseUrl : "http://127.0.0.1:8000"
-    pythonapiBaseUrl : "http://ec2-52-53-196-236.us-west-1.compute.amazonaws.com:8000"
+    pythonapiBaseUrl : "http://ec2-184-169-244-226.us-west-1.compute.amazonaws.com:8000"
   };
 export default devObj;
