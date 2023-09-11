a4d4a2a8b6686bbea437c44d631cc1c5447675cc
Aditya Jain <adityajain@Adityas-MacBook-Pro.local>
2023-09-11 11:23:57 +0530

    added .env

--- src/env/dev.js
+++ src/env/dev.js
@@ -1,7 +1,7 @@
 const devObj = {
     redirectUri: "http://localhost:3000/oauth2/redirect",
     apiBaseUrl: "http://ec2-52-9-207-36.us-west-1.compute.amazonaws.com:8080",
-    // pythonapiBaseUrl : "http://127.0.0.1:8000"
-    pythonapiBaseUrl : "http://ec2-184-169-244-226.us-west-1.compute.amazonaws.com:8000"
+    pythonapiBaseUrl : process.env.REACT_APP_PYTHON_SERVER_BASE_URL
+    // pythonapiBaseUrl : "http://ec2-184-169-244-226.us-west-1.compute.amazonaws.com:8000"
   };
 export default devObj;
